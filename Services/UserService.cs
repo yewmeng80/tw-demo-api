6bf8bd55dbecb711192eee3fafcccb839768596e
Chow Yew Meng <yewmeng80@gmail.com>
2025-10-27 07:45:54 +0800

    Create unit test for sonarqube

---
+++ Services/UserService.cs
@@ -0,0 +1,19 @@
+﻿using System.Diagnostics.Eventing.Reader;
+
+namespace tw_demo_api.Services
+{
+    public class UserService
+    {
+        public UserService() { }
+
+        public bool CreateUser(string username, string name, string email)
+        {
+            if (string.IsNullOrEmpty(username))
+            {
+                throw new ArgumentNullException("username is null or empty");
+            }
+
+            return true;
+        }
+    }
+}
