b99164e57a20acf73e589b37c68327e1ee8f264d
Chow Yew Meng <yewmeng80@gmail.com>
2025-10-27 08:47:45 +0800

    update folder structure

---
+++ tw-demo-api-test/Services/UserServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tw_demo_api.Services;
+
+namespace tw_demo_api_test.Services
+{
+    public class UserServiceTest
+    {
+        [Fact]
+        public void CreateUser_EmptyUsername_ReturnTrue()
+        {
+            var userService = new UserService();
+            Assert.Throws<ArgumentNullException>(() => userService.CreateUser("", "name", "aa@hotmail.com"));
+        }
+    }
+}
